e02e1216339168d48ab26cf9111de6ae4fd63db3
Litiano Moura <litiano.moura@yahoo.com.br>
2017-03-15 11:24:46 -0300

    adicionado coisas do deposito

---
+++ src/Enum/BoYesNoEnum.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: E-Commerce 2
+ * Date: 15/03/2017
+ * Time: 11:23
+ */
+
+namespace Litiano\Sap\Enum;
+
+
+class BoYesNoEnum
+{
+    const tNO = 0,
+        tYES = 1;
+
+}
