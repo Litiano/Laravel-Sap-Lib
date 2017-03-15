04fcb7c0d391b3d2719b9b461bb74e87f45a46d7
Litiano Moura <litiano.moura@yahoo.com.br>
2017-03-15 11:21:22 -0300

    adicionado coisas do deposito

---
+++ src/Enum/BoDepositTypeEnum.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: E-Commerce 2
+ * Date: 15/03/2017
+ * Time: 11:20
+ */
+
+namespace Litiano\Sap\Enum;
+
+
+class BoDepositTypeEnum
+{
+    const dtChecks =0,
+        dtCredit = 1,
+        dtCash = 2,
+        dtBOE = 3;
+
+}
