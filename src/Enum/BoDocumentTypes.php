41e727434edf71da4c3796153397477c5034fb77
Litiano <litiano.moura@yahoo.com.br>
2017-08-09 11:30:49 -0300

    add BoDocumentTypes

---
+++ src/Enum/BoDocumentTypes.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Litiano
+ * Date: 09/08/2017
+ * Time: 11:27
+ */
+
+namespace Litiano\Sap\Enum;
+
+
+class BoDocumentTypes
+{
+    const dDocument_Items = 0,
+    dDocument_Service = 1;
+}
