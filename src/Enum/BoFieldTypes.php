87e5deee06cc922a80812cc5a1415570eaf8c249
Litiano <litiano.moura@yahoo.com.br>
2018-02-06 17:01:13 -0300

    Add IUserFieldsMD and IValidValuesMD helpers

---
+++ src/Enum/BoFieldTypes.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Litiano
+ * Date: 06/02/2018
+ * Time: 15:37
+ */
+
+namespace Litiano\Sap\Enum;
+
+
+class BoFieldTypes
+{
+    const db_Alpha = 0,
+        db_Memo = 1,
+        db_Numeric = 2,
+        db_Date = 3,
+        db_Float = 4;
+}
---
+++ src/Enum/BoFldSubTypes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Litiano
+ * Date: 06/02/2018
+ * Time: 15:40
+ */
+
+namespace Litiano\Sap\Enum;
+
+
+class BoFldSubTypes
+{
+    const st_None = 0,
+        st_Phone = 35,
+        st_Percentage = 37,
+        st_Address = 63,
+        st_Link = 66,
+        st_Image = 73,
+        st_Measurement = 77,
+        st_Price = 80,
+        st_Quantity = 81,
+        st_Rate = 82,
+        st_Sum = 83,
+        st_Time = 84;
+}
