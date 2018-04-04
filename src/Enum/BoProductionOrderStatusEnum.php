f444cc9c1424137fa49fe9ce616ec4a3800caa97
Litiano <litiano.moura@yahoo.com.br>
2018-04-04 18:52:03 -0300

    Add BoProductionOrderStatusEnum

---
+++ src/Enum/BoProductionOrderStatusEnum.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: H4
+ * Date: 04/04/2018
+ * Time: 18:49
+ */
+
+namespace Litiano\Sap\Enum;
+
+
+class BoProductionOrderStatusEnum
+{
+    const boposPlanned = 0,
+        boposReleased = 1,
+        boposClosed = 2,
+        boposCancelled = 3;
+}
