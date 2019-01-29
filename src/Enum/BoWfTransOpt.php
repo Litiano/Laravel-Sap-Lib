972d02b54fe44d85db989a17540d1868e24f170b
Litiano Moura <litiano.moura@yahoo.com.br>
2019-01-29 19:46:58 -0300

    Added BoWfTransOpt

---
+++ src/Enum/BoWfTransOpt.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: H4
+ * Date: 29/01/2019
+ * Time: 19:37
+ */
+
+namespace Litiano\Sap\Enum;
+
+
+class BoWfTransOpt
+{
+    const wf_Commit = 0,
+        wf_RollBack = 1;
+}
