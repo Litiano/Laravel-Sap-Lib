0230d3217ef3a05686c1955ddf66e0a7fc78f22b
Litiano Moura <litiano.moura@yahoo.com.br>
2020-01-27 23:22:04 -0300

    Add tb types

--- src/Enum/BoDataServerTypes.php
+++ src/Enum/BoDataServerTypes.php
@@ -17,5 +17,8 @@ class BoDataServerTypes
         dst_MSSQL2005 = 4,
         dst_MAXDB = 5,
         dst_MSSQL2008 = 6,
-        dst_MSSQL2012 = 7;
+        dst_MSSQL2012 = 7,
+        dst_MSSQL2014 = 8,
+        dst_HANADB = 9,
+        dst_MSSQL2016 = 10;
 }
